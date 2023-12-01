bf16ca6baf1ba8e84b468576786f1f7e3ce526be
ssranjani <ranjaniblooms@gmail.com>
2023-12-01 13:29:01 -0500

    text update

--- src/main/java/com/ss/demo/controller/HelloController.java
+++ src/main/java/com/ss/demo/controller/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/demo")
     public ResponseEntity<String> display(){
-        return ResponseEntity.ok("Hello!");
+        return ResponseEntity.ok("Welcome!");
     }
 }
