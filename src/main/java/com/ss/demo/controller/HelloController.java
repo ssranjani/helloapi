7774b0dc9ec9f9699bd8c4b2344370d01200b313
ssranjani <ranjaniblooms@gmail.com>
2023-12-01 11:33:11 -0500

    new package

---
+++ src/main/java/com/ss/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.ss.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/demo")
+    public ResponseEntity<String> display(){
+        return ResponseEntity.ok("Hello!");
+    }
+}
